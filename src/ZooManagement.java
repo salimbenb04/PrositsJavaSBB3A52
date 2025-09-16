337d347aeca8a9946c9753fdb56a0170dc732360
salim <salim.benbrahim@esprit.tn>
2025-09-16 12:00:55 +0100

    prosit 1

---
+++ src/ZooManagement.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ZooManagement {
+
+    public static void main(String[] args) {
+
+        //instruction 1
+        int nbrCages=20;
+        String zooName = "my zoo";
+
+        System.out.println(zooName+" comporte"+nbrCages+" cages");
+
+        //instruction 2
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("entrez le nom du zoo");
+            zooName = sc.nextLine();
+        }while(zooName.isEmpty());
+
+        do {
+            System.out.println("entrez le nombre des cages");
+            nbrCages = sc.nextInt();
+        }while(nbrCages<=0);
+
+        System.out.println(zooName+" comporte "+nbrCages+" cages");
+
+    }
+
+}
