02d3ebdde6485dc9d3ca5ef5d2447cf10987c09a
Salim Ben Brahim <162966670+salimbenb04@users.noreply.github.com>
2025-11-04 21:54:34 +0100

    Delete src directory

--- src/tn/esprit/gestionzoo/entities/ZooFullException.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.gestionzoo.entities;
-
-public class ZooFullException extends Exception {
-    public ZooFullException() {}
-    public ZooFullException(String message) {
-        super(message);
-    }
-}
